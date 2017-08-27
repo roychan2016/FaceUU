ceadfd884c17d80642166837d3c6085ee15d30c0
roychan2016 <roychan2009@126.com>
2017-08-27 18:12:43 +0800

    添加美颜的动画

--- app/src/main/java/com/xuweichen/faceuu/MainActivity.java
+++ app/src/main/java/com/xuweichen/faceuu/MainActivity.java
@@ -8,7 +8,12 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.xuweichen.imagefilter.FaceEngine;
@@ -23,9 +28,19 @@ public class MainActivity extends AppCompatActivity {
     private FaceEngine faceEngine;
 
     private CameraGLSurface cameraGLSurface;
+
     private ImageView photoButton;
+
     private ImageView beautifulButton;
     private ImageView filterButton;
+
+    private LinearLayout beautifulPanel;
+    private TextView noBeautiful;
+    private TextView oneBeautiful;
+    private TextView twoBeautiful;
+    private TextView threeBeautiful;
+    private TextView fourBeautiful;
+    private TextView fiveBeautiful;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,13 +60,22 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         cameraGLSurface = (CameraGLSurface) findViewById(R.id.camera_surface);
         faceEngine = FaceEngine.Builder.build(cameraGLSurface);
+        cameraGLSurface.setOnClickListener(onClickListener);
 
         photoButton = (ImageView) findViewById(R.id.photo_button);
-        photoButton.setOnClickListener(buttonClickListener);
+        photoButton.setOnClickListener(onClickListener);
+
         beautifulButton = (ImageView) findViewById(R.id.beautiful_button);
-        beautifulButton.setOnClickListener(buttonClickListener);
+        beautifulButton.setOnClickListener(onClickListener);
         filterButton = (ImageView) findViewById(R.id.filter_button);
-        filterButton.setOnClickListener(buttonClickListener);
+        filterButton.setOnClickListener(onClickListener);
+
+        beautifulPanel = (LinearLayout) findViewById(R.id.beautiful_layout);
+        oneBeautiful = (TextView) findViewById(R.id.one_beautiful);
+        twoBeautiful = (TextView) findViewById(R.id.two_beautiful);
+        threeBeautiful = (TextView) findViewById(R.id.three_beautiful);
+        fourBeautiful = (TextView) findViewById(R.id.four_beautiful);
+        fiveBeautiful = (TextView) findViewById(R.id.five_beautiful);
     }
 
     @Override
@@ -66,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
         //if (null != cameraGLSurface) cameraGLSurface.onPause();
     }
 
-    private View.OnClickListener buttonClickListener = new View.OnClickListener() {
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             switch (view.getId()) {
@@ -80,8 +104,11 @@ public class MainActivity extends AppCompatActivity {
                         takePhoto();
                     }
                     break;
+                case R.id.camera_surface:
+                    showBeautifulPanel(false);
+                    break;
                 case R.id.beautiful_button:
-                    Toast.makeText(MainActivity.this, "开启美颜效果", Toast.LENGTH_SHORT).show();
+                    showBeautifulPanel(true);
                     break;
                 case R.id.filter_button:
                     Toast.makeText(MainActivity.this, "打开滤镜面板", Toast.LENGTH_SHORT).show();
@@ -116,5 +143,29 @@ public class MainActivity extends AppCompatActivity {
         if (grantResults.length == 1 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
             super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+    }
+
+    private void showBeautifulPanel(boolean show) {
+        beautifulButton.setVisibility(show ? View.GONE : View.VISIBLE);
+        filterButton.setVisibility(show ? View.GONE : View.VISIBLE);
+
+        ScaleAnimation photoBtnAnim = new ScaleAnimation(
+                show ? 1.0f : 0.75f, show ? 0.75f : 1.0f,
+                show ? 1.0f : 0.75f, show ? 0.75f : 1.0f,
+                Animation.RELATIVE_TO_SELF, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f
+        );
+        photoBtnAnim.setDuration(500);
+        photoBtnAnim.setFillAfter(true);
+        photoButton.startAnimation(photoBtnAnim);
+
+        TranslateAnimation beautifulPanelAnim = new TranslateAnimation(
+                Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
+                0.0f, Animation.RELATIVE_TO_SELF, show ? 1.0f : 0.0f,
+                Animation.RELATIVE_TO_SELF, show ? 0.0f : 1.0f);
+        beautifulPanelAnim.setDuration(500);
+        beautifulPanelAnim.setFillAfter(true);
+
+        beautifulPanel.setVisibility(View.VISIBLE);
+        beautifulPanel.startAnimation(beautifulPanelAnim);
     }
 }
